8b4f40aa82a66235228f7cc9e67101a1d32823ff
irinache <irina.che333@gmail.com>
2020-03-14 14:17:55 +0200

    Added categories to Showcase.vue instead items

--- src/components/Showcase.vue
+++ src/components/Showcase.vue
@@ -8,41 +8,20 @@
 					</h2>
 				</div>
 			</div>	
-			<div class="row">	
-				
-						<div class="col-lg-3 col-md-4 col-sm-6 item-container text-left" v-for="item in items" v-bind:key="item.id">
-							<router-link to="/item_list/item" class="item-card">						
-									<h3>
-										{{ item.name }}					
-									</h3>
-									<img class="item-img" src="../assets/images/card-img.png" alt="Изображение товара">
-									<div class="info">
-										Оптом и в розницу
-									</div>
-									<div class="info">
-										Под заказ, 7 дней
-									</div>
-									<div class="price">
-										от 186 грн/кв.м.
-									</div>
-									<div class="solid-button small-button" @click.prevent="showAddedToCartModal">
-										Купить
-									</div>
-									<div class="small-button brdr-btn-main-color" @click.prevent="showModalCallMeBack">
-										Перезвоните мне
-									</div>							
-							</router-link>
-						</div>
-					
-			</div>	
 			<div class="row">
-				<div class="col-md-12">
-					<router-link to="/categories" class="button brdr-btn-dark-color btn-pos">
-						Все товары и услуги
-					</router-link>
+				<div class="col-lg-4 col-sm-12 category-margin" v-for="c in categories" v-bind:key="c.id">
+					<router-link to="/item_list" class="category">
+						<div class="counter">
+							{{c.items_count}}
+						</div>
+						<img src="../assets/images/card-img.png" alt="" class="image">	
+						<h3>
+							{{c.name}}
+						</h3>						
+					</router-link>						
 				</div>
-			</div>				
-		</div>	
+			</div>					
+		</div>
 	</div>	
 </template>
 
@@ -50,76 +29,125 @@
 	export default{
 		data(){
 			return{
-				items:
-				[
-				{
-					id: 1,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 2,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 3,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 4,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 5,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 6,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 7,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},
-				{
-					id: 8,
-					name: "Фальцевая кровля",
-					img: "",
-					price: "от 186 грн/кв.м.",
-				},				
+				categories: [
+					{
+						id: 1,
+						items_count: 17,
+						name: "Металлочерепица",
+					},
+					{
+						id: 2,
+						items_count: 1,
+						name: "Фальцевая кровля",
+					},
+					{
+						id: 3,
+						items_count: 62,
+						name: "Битумная черепица",
+					},
+					{
+						id: 4,
+						items_count: 7,
+						name: "Водосточные системы",
+					},
+					{
+						id: 5,
+						items_count: 14,
+						name: "Профнастил",
+					},
+					{
+						id: 6,
+						items_count: 2,
+						name: "Подкровельные пленки",
+					},
+					{
+						id: 7,
+						items_count: 17,
+						name: "Раздвижные решетки на окна и двери",
+					},
+					{
+						id: 8,
+						items_count: 17,
+						name: "Вентиляционные выходы",
+					},
+					{
+						id: 9,
+						items_count: 17,
+						name: "Кровельные уплотнители для труб ",
+					},
+					{
+						id: 10,
+						items_count: 17,
+						name: "Композитная черепица ",
+					},
+					{
+						id: 11,
+						items_count: 17,
+						name: "FAKRO",
+					},
+					{
+						id: 12,
+						items_count: 17,
+						name: "VELUX",
+					},
 				]
 			}
-		},
-		methods:{
-			showAddedToCartModal() {    			
-				this.$root.$emit('modal-atc-on', true);		        	
-			},
-			showModalCallMeBack() {    			
-				this.$root.$emit('modal-cmb-on', true);		        	
-			},
 		}
 	}
 </script>
 
 <style lang="less" scoped>
+
 @import '../assets/styles/index.less';
-.btn-pos{
-	margin: 60px auto 0 auto;
+h3{
+	max-width: 312px;
+	margin-right: auto;
+	margin-left: auto;
 }
-.small-button{
-	margin-top: 15px;
+h3:after{
+	content: none; 
+}
+h3:before{
+	content: " ";
+  display: block;
+  width: 80px;
+  height: 1px;  
+  margin: 0 auto;
+  margin-bottom: 10px;
+  background-color: @main_color;
+}
+.category-margin{
+	padding: 15px;
+}
+.image{
+	width: 100%;
+	height: 318px;
+	object-fit: contain;
+	background-color: @medium-bg;
+	margin-bottom: 15px;
+}
+.category{
+	display: block;
+	padding: 15px 15px 35px 15px;
+	width: 100%;
+	height: fir-content;
+	background-color: @light_text;
+	color: @dark-bg;
+	text-align: center;
+	border: 1px solid transparent;
+}
+.category:hover{
+	color: @main_color;
+	border: 1px solid @main_color;
+}
+.counter{
+	position: absolute;
+	top: 15px;
+	right: 15px;
+	padding: 15px 18px;	
+	min-width: 55px;
+	background-color: @main_color;
+	font-weight: bold;
+	color: @light_text;
 }
 </style>
